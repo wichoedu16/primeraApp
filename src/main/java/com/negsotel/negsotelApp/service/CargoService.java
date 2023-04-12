0ba0a61cb1eecac7788a0c2ffbb11e6c0842c23f
wichoedu16 <wichoedu16@gmail.com>
2023-04-12 12:38:44 -0500

    modificar repository

--- src/main/java/com/negsotel/negsotelApp/service/CargoService.java
+++ src/main/java/com/negsotel/negsotelApp/service/CargoService.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Service
-public class CargoService implements CargoRepositoryß{
+public class CargoService {
     @Autowired
     CargoRepository cargoRepository;
 
